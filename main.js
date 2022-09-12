8cb1a617801f92e4e364daa44cd6be152278453f
Ddsans <davidgeosantos@gmail.com>
2022-09-12 08:37:17 -0300

    aulajs

---
+++ main.js
@@ -0,0 +1,51 @@
+// let nome = "David";
+// let diga = "Olá!";
+
+// console.log(diga + nome);
+
+// nome = "Laysa";
+
+// console.log (diga + nome);
+
+// //exercicio 2:
+
+// let x = "David";
+// let y = 28;
+
+// console.log("O usuário de nome: ", x, "tem", y, "anos");
+
+// x = "Laysa";
+// y =  19;
+
+// console.log("O usuário de nome: ", x, "tem", y, "anos");
+
+// console.log(typeof y);
+// console.log(typeof x);
+
+// let cliente = "Alcides Garcia";
+// let conta = "67768734";
+// let saldo = 322.95;
+
+// console.log("Cliente: " + cliente);
+// console.log("Conta: " + conta);
+// console.log("saldo: " + saldo);
+
+// // console.log(typeof saldo);
+
+// let deposito = 50.00;
+// let saldoAtual = saldo + deposito;
+
+// console.log("Novo deposito na conta " + conta + " saldo atualizado " + saldoAtual);
+
+// Exiba a nota de 4 bimestre e calcule a média
+
+let bimestre1 = 6.5;
+let bimestre2 = 8;
+let bimestre3 = 7;
+let bimestre4 = 9.5;
+
+console.log("Nota Primeiro bimestre: " + bimestre1)
+console.log("Nota Segundo bimestre: " + bimestre2)
+console.log("Nota Terceiro bimestre: " + bimestre3)
+console.log("Nota Quarto bimestre: " + bimestre4)
+console.log("Média das notas: " + (bimestre1 + bimestre2 + bimestre3 + bimestre4)/4)
